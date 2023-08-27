a16d5c729f9709b704fe4e4993c55e621821336b
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-08-28 01:59:46 +0430

    Creating Database

---
+++ API/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+	public class Context: IdentityDbContext<User>
+	{
+        public Context(DbContextOptions<Context> options): base(options)
+        {
+            
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +13,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
